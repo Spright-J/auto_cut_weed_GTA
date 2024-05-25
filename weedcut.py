17bcb0cbaadea0c75b64cbd07dddff38f3d94595
Jaxon H <jaxonthgit@gmail.com>
2024-05-24 20:44:03 -0400

    added wait for game to recognise input

--- weedcut.py
+++ weedcut.py
@@ -2,8 +2,8 @@ from pynput.mouse import Button, Controller as MouseController
 from pynput.keyboard import Key, Controller as KeyboardController
 import time
 
-cycleTime = 10 # CAN BE CHANGED TO FIT THE TIMMINGS
-Weed_Cut_Time = 1 #CHANGE ME BASED ON TYPE OF WEED CUTTING
+cycleTime = .1 # CAN BE CHANGED TO FIT THE TIMMINGS
+Weed_Cut_Time = 3.4 #CHANGE ME BASED ON TYPE OF WEED CUTTING
 
 # Create mouse and keyboard controller objects
 mouse = MouseController()
@@ -15,7 +15,7 @@ def CutWeed(e_time):
     print('Left Alt key pressed')
 
     mouse.press(Button.right)
-    time.sleep(1)
+    time.sleep(.1)
     mouse.release(Button.right)
     print('Right click')
 
@@ -23,19 +23,23 @@ def CutWeed(e_time):
     print('Mouse moved by 50 pixels')
 
     mouse.press(Button.left)
-    time.sleep(1)
+    time.sleep(.1)
     mouse.release(Button.left)    
     print('Left click')
+    keyboard.release(Key.alt_l)
+
 
     time.sleep(e_time)
-    keyboard.tap('e')
+    keyboard.press('e')
+    time.sleep(.1)
+    keyboard.release("e")
     print('First Cut!')
 
     time.sleep(e_time)
+    keyboard.press("e")
+    time.sleep(.1)
+    keyboard.release("e")
     print('Second Cut!')
-
-    keyboard.release(Key.alt_l)
-    print('Left Alt key released')
 
 
 def main():
